7ac1cdf27fc294f138c19de958e8f53d06483f0b
Screathles <80972359+Screathles@users.noreply.github.com>
2023-01-16 13:28:31 +0100

    .

---
+++ src/main/java/net/mrys/mysterious_mechanism/mysterious_mechanism.java
@@ -0,0 +1,34 @@
+package net.mrys.mysterious_mechanism;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(mysterious_mechanism.MOD_ID)
+public class mysterious_mechanism {
+    public static final String MOD_ID = "mysterious_mechanism";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public mysterious_mechanism() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
